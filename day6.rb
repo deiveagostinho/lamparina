ca915099459bee24befbb14f146a2d467b178df8
Deive Agostinho <deiviane.agostinho@gmail.com>
2014-05-07 22:02:43 -0300

    day6

---
+++ day6.rb
@@ -0,0 +1,7 @@
+print "Como você deve ser feliz?"
+return_= gets.chomp
+puts ("Não é #{return_} que lhe fará feliz...")
+sorvete = 70.00
+dinheiro_da_mae = 20.00
+vitoria = sorvete + dinheiro_da_mae
+puts "Você será feliz se sua mãe lhe der #{dinheiro_da_mae} e seu pai #{sorvete} para comprar sorvete"
